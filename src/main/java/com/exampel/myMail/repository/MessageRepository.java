bc1d9c9db191a6677acc1863f64bdce2922e8851
mporsyak <47520769+mporsyak@users.noreply.github.com>
2019-10-16 22:41:31 +0300

    Удалить не используемый код

--- src/main/java/com/exampel/myMail/repository/MessageRepository.java
+++ src/main/java/com/exampel/myMail/repository/MessageRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MessageRepository extends CrudRepository<Message, Long>{
-    Message findByContent(String content);
     List<Message> findByUserSenderAndUserRecip(User userSender, User userRecip);
 }
